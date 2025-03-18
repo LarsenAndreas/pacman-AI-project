38edce8e325a996d56469b1415bd00f55eb48150
Martin Sig Nørbjerg <mart547n@outlook.dk>
2025-03-18 13:50:29 +0100

    Added code for constructing input matrix

--- planningAgents.py
+++ planningAgents.py
@@ -5,6 +5,8 @@ import random
 import game
 import util
 from pacman import GameState
+import numpy as np 
+from numpy.typing import ArrayLike
 
 class PlanningAgent(game.Agent):
     "An agent that turns left at every opportunity"
@@ -31,3 +33,19 @@ class PlanningAgent(game.Agent):
             print(ghost)
 
         return Directions.STOP
+
+    def constructInputMatrix(self, state: GameState) -> ArrayLike:
+        """Constructs an input matrix based on the given state."""
+        #matrix = np.zeros((N, M), dtype="float32")
+        matrix = np.array(state.getWalls().data, dtype="float32")
+
+        food = np.array(state.getFood().data, dtype="float32")
+        matrix[:food.shape[0], :food.shape[1]] += 2 * food
+        
+        (i, j) = state.getPacmanPosition()
+        matrix[i, j] = 3
+
+        for (i, j) in state.getGhostPositions():
+            matrix[int(i), int(j)] = 4
+
+        return matrix
